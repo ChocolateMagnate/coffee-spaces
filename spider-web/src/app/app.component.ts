7de3b6774b33e6a7639d5553801e68d6880ba9ea
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-07-20 12:53:56 +0000

    worked on login system and added a todo list.

--- spider-web/src/app/app.component.ts
+++ spider-web/src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Input } from '@angular/core';
+//import { AuthService } from './auth.service';
+/*TO-DO:
+1. Resolve AuthService import;
+2. Implement login-system;
+3. Implement registeration;
+4. Implement bug reports and SQL.*/
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spider-web';
+  /*Properties have no initializer and is not definitely assigned in the constructor:
+  @Input() isLoggedIn: boolean;
+  @Input() username: string;
+  @Input() position: string;
+  @Input() password: string;*/
+
+  onLogin(): void {
+
+  }
 }
