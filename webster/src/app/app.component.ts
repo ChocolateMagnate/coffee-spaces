9b921767a828d65dfdb41e15930d6368aa97e583
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-07-25 14:25:58 +0000

    updated the project,

--- webster/src/app/app.component.ts
+++ webster/src/app/app.component.ts
@@ -10,23 +10,30 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'webster';
   constructor(private router: Router, /*private authService: AuthService*/) {
-    console.log("App component loaded");
     window.addEventListener('scroll', this.reveal);
+    console.log("App component loaded");
   }
 
   reveal() {
     //Resolve this method.
     console.log("Scrolling");
-    let reveals = document.querySelectorAll('.reveal');
+    let reveals = document.getElementsByClassName("hidden");
+    console.log(reveals);
+    let windowHeight = window.innerHeight; //Get the window height.
+    let elementVisible = 150; //The element is visible if it is 150px from the top of the screen.
     for (let step = 0; step < reveals.length; step++) {
-      let windowHeight = window.innerHeight;
-      let topItem = reveals[step].getBoundingClientRect().top;
-      let elementVisible = 150;
-      if (topItem < windowHeight - elementVisible) {
-        reveals[step].classList.add('unfold');
-        console.log(reveals[step] + "activated");
-      } else {
-        reveals[step].classList.remove('unfold');
+      let topItem = reveals[step].getBoundingClientRect().top; //Get the top coordinate of the element.
+      console.log("Window height: " + windowHeight);
+      console.log("Top item: " + topItem);
+      console.log("Element visible: " + elementVisible);
+      if (topItem < windowHeight + elementVisible) {
+        reveals[step].classList.remove("hidden");
+        reveals[step].classList.add("reveal");
+      }
+      if (topItem < windowHeight - elementVisible - 1000) {
+        reveals[step].classList.remove("hidden");
+        reveals[step].classList.add('reveal');
+        console.log(reveals[step] + " activated");
       }
     }
   }
