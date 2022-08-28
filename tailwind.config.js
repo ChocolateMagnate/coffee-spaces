ad4df0a8e6930da7cd21c4e1361112a0c45b7982
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-08-28 12:29:45 +0000

    familiarised myself with Tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/webster/src/*.html", "/webster/src/*/*.html", 
-  "webster/src/app/code/code.component.html"],
+  content: ["webster/src/app/code/code.component.html",
+  "webster/src/app/development/development.component.html",
+  "/workspaces/spider-web/webster/src/app/welcome/welcome.component.html"],
   theme: {
     extend: {},
   },
