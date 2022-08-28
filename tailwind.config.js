6978b057774295a5901bab55fd9a0b2f5fc2203d
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-08-28 09:43:08 +0000

    installed Tailwind in the project.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/webster/src/*.html", "/webster/src/*/*.html", 
+  "webster/src/app/code/code.component.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
