59185a2cd0d874a3622726e4a0c9846f335fba58
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-09-04 13:03:59 +0000

    changed the project type to Blazor WASM.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,11 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using spider_web.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using coffee_spaces;
 
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var builder = WebApplication.CreateBuilder(args);
-//string[] origins = new string[3] { "http://localhost:4200", "http://localhost:7093", "http://localhost:5226" };
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Allow-local-host", builder =>
-    {
-        builder.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-                      });
-});
-
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddMvc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseCors();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+await builder.Build().RunAsync();
--- README.md
+++
@@ -1,6 +0,0 @@
-# Spider Web
-A web implementation of a bugtracker.
-Features:
-* closed testing enviornment for enterprise applications;
-* roles that manage permissions and privilages;
-* rich bug-reporting attachments (description, hashtags, pictures and video, etc.)
